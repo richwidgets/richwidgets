4d75e2845b226de8207f76458fb9badb9a338507
Lukas Fryc <lfryc@redhat.com>
2013-07-31 11:49:47 +0200

    upgrade to Karma 0.9.6 and added karma-spec-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,82 +1,89 @@
-// Karma configuration
-// Generated on Sun Apr 14 2013 18:31:17 GMT+0200 (CEST)
+module.exports = function(config) {
+
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    frameworks: ['jasmine', 'requirejs'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'components/jasmine-jquery/lib/jasmine-jquery.js',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+      'test/test-main.js',
+
+      {pattern: 'components/jquery/jquery.js', included: false},
+      {pattern: 'components/jquery-ui/ui/jquery-ui.js', included: false},
+      {pattern: 'components/dom-compare/**/*.js', included: false},
+      {pattern: 'components/syn/**/*.js', included: false},
+      {pattern: 'dist/assets/**/*.css', included: false},
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
-  'components/jasmine-jquery/lib/jasmine-jquery.js',
-
-  {pattern: 'components/jquery/jquery.js', included: false},
-  {pattern: 'components/jquery-ui/ui/jquery-ui.js', included: false},
-  {pattern: 'components/dom-compare/**/*.js', included: false},
-  {pattern: 'components/syn/**/*.js', included: false},
-  {pattern: 'dist/assets/**/*.css', included: false},
+      {pattern: 'src/**/*.js', included: false},
+      {pattern: 'test/utils/*.js', included: false},
+      {pattern: 'test/**/*.spec.js', included: false},
+      {pattern: 'test/**/*.html', included: false}
+    ],
 
 
-  {pattern: 'src/**/*.js', included: false},
-  {pattern: 'test/utils/*.js', included: false},
-  {pattern: 'test/**/*.spec.js', included: false},
-  {pattern: 'test/**/*.html', included: false},
-
-  'test/test-main.js'
-];
+    preprocessors: {
+      // do not preprocess HTML files with html2js
+      '**/*.html': []
+    },
 
 
-// list of files to exclude
-exclude = [
+    // list of files to exclude
+    exclude: [
 
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-reporters = ['progress'];
+    ],
 
 
-// web server port
-port = 9876;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['spec'],
 
 
-// cli runner port
-runnerPort = 9100;
+    // web server port
+    port: 9876,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // cli runner port
+    runnerPort: 9100,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+
+  });
+
+};
