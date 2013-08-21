fe21bf687e4eefa1e297341d9bf24ff221cc6a6c
Brian Leathem <bleathem@gmail.com>
2013-08-20 22:34:56 -0700

    Added interaction tests for the all the orderingList buttons

--- test/widgets/select/ordering-list-interaction.js
+++ test/widgets/select/ordering-list-interaction.js
@@ -25,7 +25,7 @@ define(['widget-test-base', 'jquery', 'jquery-ui', 'src/widgets/select/orderingL
         original = fixture.clone();
       });
 
-      describe('move first to last:', function () {
+      describe('buttons: ', function () {
         it('move first to last:', function () {
           // given
           element.orderingList({});
@@ -51,10 +51,106 @@ define(['widget-test-base', 'jquery', 'jquery-ui', 'src/widgets/select/orderingL
 
           waitsFor(function() {
             return fixture.find('.ui-selectee').last().data('key') === firstItem.data('key');
-          }, 'first item should be moved to end of list', 500);
+          }, 'item should be moved to end of list', 500);
 
           runs(function() {
             expect(widget._dumpState().orderedKeys).toEqual([2, 3, 4, 5, 6, 7, 8, 1]);
+          });
+        });
+
+        it('move last to first:', function () {
+          // given
+          element.orderingList({});
+          var widget = element.data('orderingList');
+          expect(widget._dumpState().orderedKeys).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+
+          // when
+          var lastItem = fixture.find('.ui-selectee').last();
+
+          //then
+          runs(function () {
+            lastItem.trigger('mousedown');
+            lastItem.trigger('mouseup');
+          });
+
+          waitsFor(function() {
+            return lastItem.hasClass('ui-selected');
+          }, "item should be selected", 500);
+
+          runs(function () {
+            fixture.find('.buttonColumn .first').first().click();
+          });
+
+          waitsFor(function() {
+            return fixture.find('.ui-selectee').first().data('key') === lastItem.data('key');
+          }, 'first item should be moved to top of list', 500);
+
+          runs(function() {
+            expect(widget._dumpState().orderedKeys).toEqual([8, 1, 2, 3, 4, 5, 6, 7]);
+          });
+        });
+
+        it('move 2nd down to 3rd:', function () {
+          // given
+          element.orderingList({});
+          var widget = element.data('orderingList');
+          expect(widget._dumpState().orderedKeys).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+
+          // when
+          var item = $(fixture.find('.ui-selectee').get(1));
+
+          //then
+          runs(function () {
+            item.trigger('mousedown');
+            item.trigger('mouseup');
+          });
+
+          waitsFor(function() {
+            return item.hasClass('ui-selected');
+          }, "item should be selected", 500);
+
+          runs(function () {
+            fixture.find('.buttonColumn .down').first().click();
+          });
+
+          waitsFor(function() {
+            return $(fixture.find('.ui-selectee').get(2)).data('key') === item.data('key');
+          }, 'first item should be moved down', 500);
+
+          runs(function() {
+            expect(widget._dumpState().orderedKeys).toEqual([1, 3, 2, 4, 5, 6, 7, 8]);
+          });
+        });
+
+        it('move 4th down to 3rd:', function () {
+          // given
+          element.orderingList({});
+          var widget = element.data('orderingList');
+          expect(widget._dumpState().orderedKeys).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+
+          // when
+          var item = $(fixture.find('.ui-selectee').get(3));
+
+          //then
+          runs(function () {
+            item.trigger('mousedown');
+            item.trigger('mouseup');
+          });
+
+          waitsFor(function() {
+            return item.hasClass('ui-selected');
+          }, "item should be selected", 500);
+
+          runs(function () {
+            fixture.find('.buttonColumn .up').first().click();
+          });
+
+          waitsFor(function() {
+            return $(fixture.find('.ui-selectee').get(2)).data('key') === item.data('key');
+          }, 'first item should be moved up', 500);
+
+          runs(function() {
+            expect(widget._dumpState().orderedKeys).toEqual([1, 2, 4, 3 , 5, 6, 7, 8]);
           });
         });
       });
