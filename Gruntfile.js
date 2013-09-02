26c6e1bc650bca93a80ae7ca0dd00226ba0fcd31
Lukas Macko <llukas3@gmail.com>
2013-09-02 11:49:00 +0200

    Fix flot version in dist to 0.8.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function (grunt) {
                 {
                     expand: true,
                     cwd: "<%= config.dir.components.flot.tooltip %>/js",
-                    src: ["**.js"],
+                    src: ["**.js","!jquery.flot.js"],
                     dest: "<%= config.dir.dist.assets %>/flot"
                 }
             ]
