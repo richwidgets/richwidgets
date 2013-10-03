a91dd76dd262ed4e0831a5152f669913a46dc4de
Lukas Macko <llukas3@gmail.com>
2013-10-03 15:37:58 +0200

    chart.js comments added

--- src/widgets/output/chart.js
+++ src/widgets/output/chart.js
@@ -19,6 +19,7 @@
             }
         },
 
+        // Default options for pie chart
         pieDefaults:{
             series:{
                 pie:{
@@ -34,6 +35,8 @@
                 defaultTheme: false
             }
         },
+
+        //Default options charts with a date data type on xaxis
         dateDefaults:{
             xaxis:{
                 mode:'time',
@@ -49,15 +52,117 @@
             this._registerListeners();
         },
 
+        //if chart is created by JSF component and charttype option is set, there is need to update some options.
         _handleTypeDependentOptions:function(){
 
             if(this.options.charttype==='pie'){
                 this.options = $.extend(this.options,this.pieDefaults);
+                //transform data from
+                // [[{data:1, label:"label1"},{data:2,label:"label2"},...]]
+                //to
+                // [{data:1, label:"label1"},{data:2,label:"label2"},...]
                 this.options.data = this.options.data[0]; //pie chart data should not be in a collection
             }
             else if(this.options.charttype==='bar'){
                 if(this.options.xtype==='class java.lang.String'){
                     //category bar chart
+                    /*transformation data from
+                     [
+                        {   data:   {"Key1":11, "Key2":21, "Key3": 31},
+                            label:  "label1",
+                            bars:   {show:true}
+                        },
+
+                        {   data:   {"Key1":12,"Key2":22, "Key3": 32},
+                            label:  "label2",
+                            bars:   {show:true}
+                        },
+
+                        {   data:   {"Key1":13,"Key2":23,"Key3":33},
+                            label:  "label3",
+                            bars:   {show:true}
+                        },
+                        {   data:   {"Key1":14,"Key2":24,"Key4":44},
+                            label:  "label4",
+                            bars:   {show:true}
+                        }
+                      ]
+
+                     to the form:
+
+
+                     [
+                        {
+                            data:   [
+                                        [0,11],[1,21],[2, 31]
+                                    ],
+                            label:  "label1",
+                            bars: {
+                                    show:true,
+                                    order=0
+                            }
+                        },
+                        {
+                            data:   [
+                                        [0,12],[1,22],[2, 32]
+                                    ],
+                            label:  "label2",
+                            bars: {
+                                show:true,
+                                order=1
+                            }
+                        },
+                        {
+                            data:   [
+                                        [0,13],[1,23],[2, 33]
+                                    ],
+                            label:  "label3",
+                            bars: {
+                               show:true,
+                               order=2
+                            }
+                        },
+                        {
+                            data:   [
+                                        [0,14],[1,24],[2, 0]
+                                    ],
+                            label:  "label4",
+                            bars: {
+                                show:true,
+                                order=3
+                            }
+                        },
+
+
+
+                     ]
+
+                     and create array ticks
+
+                     ticks = [[0,"Key1"],[1,"Key2"],[2,"Key3"]] and assign this array to the this.options.xaxis
+
+
+                     according to the following rules:
+                     data:
+                         - select all keys used in the first series (set)
+                         - in the following series find the keys selected in first
+                            -missing consider to be 0   (ie. "Key3" in fourth series)
+                            -ignore additional          (ie. "Key5" in fourth series)
+                     keys:
+                         COUNTER=0
+                         - for each key used in the first series
+                               add couple [COUNTER, KEY_LABEL] to ticks
+                               COUNTER++
+                     order:
+                         order of the series
+
+
+
+
+
+                     */
+
+
                     var ticks=[],keys=[],first=true,order=0;
 
                     for (var index in this.options.data){//loop through data series
@@ -78,6 +183,9 @@
                                 if(this.options.data[index].data[loopKey]){
                                     convertedData.push([cnt,this.options.data[index].data[loopKey]]);
                                 }
+                                else{
+                                    convertedData.push([cnt,0]);
+                                }
                                 cnt++;
                             }
                         }
@@ -91,6 +199,7 @@
 
                     }
 
+                    //add ticks to the options
                     this.options.xaxis= $.extend(this.options.xaxis,{
                         ticks:ticks,
                         tickLength:0,
@@ -98,6 +207,7 @@
                         tickFormatter:function(value,axis){return axis.ticks[value].label;}
                     });
 
+                    //options for all bars
                     this.options.bars= $.extend(this.options.bars,{
                         show: true,
                         barWidth: 0.2,
@@ -124,7 +234,9 @@
             // In jQuery UI 1.9 and above, you use the _super method instead
             this._super( '_setOption', key, value );
 
-            var redraw=true;
+            var redraw=true; //variable decides whether redraw of the chart is required
+
+            //the change of a handler does not require chart to be redrawn
             switch( key ) {
                 case 'zoom':
                 case 'handlers':
@@ -140,6 +252,7 @@
             }
         },
 
+        //method draws a chart if opts given it is used as options for the chart. Otherwise this.options is used
         _draw:function(opts){
             if(opts){
                 this.plot = $.plot(this.element,opts.data,opts);
@@ -149,6 +262,7 @@
             }
         },
 
+        //bind listeners
         _registerListeners:function(){
             if(this.options.zoom){
                 this.element.bind('plotselected',this._getZoomFunction(this,this.element,this.options));
@@ -173,11 +287,13 @@
                     };
 
                     //server-side
-                    options.handlers.eventFunction(event,'plotclick',
-                        event.data.seriesIndex,
-                        event.data.dataIndex,
-                        event.data.x,
-                        event.data.y);
+                    if(options.handlers && options.handlers.eventFunction){
+                        options.handlers.eventFunction(event,'plotclick',
+                            event.data.seriesIndex,
+                            event.data.dataIndex,
+                            event.data.x,
+                            event.data.y);
+                    }
 
                     //client-side
                     if(options.handlers && options.handlers['onplotclick']){
@@ -191,6 +307,7 @@
             };
         },
 
+        //function handles plothover event. it calls client-side and particular series handlers if set.
         _getPlotHoverHandler: function(options,element){
             return function(event,mouse,item){
                 if(item !== null){
@@ -202,10 +319,12 @@
                         y: item.datapoint[1],
                         item:item
                     };
+
                     //client-side
                     if(options.handlers && options.handlers['onplothover']){
                         options.handlers['onplothover'].call(element,event);
                     }
+
                     //client-side particular series handler
                     if(options.particularSeriesHandlers && options.particularSeriesHandlers['onplothover'][event.data.seriesIndex]){
                         options.particularSeriesHandlers['onplothover'][event.data.seriesIndex].call(element,event);
@@ -215,7 +334,7 @@
         },
 
 
-
+        //function handling zooming
         _getZoomFunction : function(widget,element,options){
             return function(event,ranges){
                 // clamp the zooming to prevent eternal zoom
@@ -242,16 +361,19 @@
             //redraw chart without axis ranges
             this._draw();
         },
-        
-        getPlotObject: function(){
-            return this.plot;
-	},
 
+
+        //returns chart object
+        getPlotObject: function(){
+	        return this.plot;
+	    },
+
+        //remove chart handler bindings
         _unbind:function(){
-            this.element.unbind('plotclick');
-            this.element.unbind('plothover');
-            this.element.unbind('plotselected');
-            this.element.unbind('mouseout');
+            this.element.unbind("plotclick");
+            this.element.unbind("plothover");
+            this.element.unbind("plotselected");
+            this.element.unbind("mouseout");
         },
 
         // Use the destroy method to clean up any modifications your widget has made to the DOM
