e0e0a27515c5284ddc077d93bb25d065eb0f17e5
Lukas Macko <lmacko@redhat.com>
2013-10-17 18:03:59 +0200

    chart examples responsive row design

---
+++ src/demos/pages/output/charts/pie-chart.js
@@ -0,0 +1,7 @@
+$(document.getElementById("pie")).chart(
+    {
+        data: [[{"data":12500746,"label":"Service sector"},{"data":188217,"label":"Agricultural sector"},{"data":2995787,"label":"Industrial sector"}]],
+        "yaxis":{},"ytype":"class java.lang.Number","xaxis":{},"legend":{"sorted":"ascending"},"zoom":false,"xtype":"class java.lang.String","charttype":"pie"
+    }
+
+);
