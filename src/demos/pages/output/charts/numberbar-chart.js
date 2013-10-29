5c15067d7233eb49b9101402c370039c34a45422
Lukas Macko <llukas3@gmail.com>
2013-10-29 13:54:39 +0100

    max width for chart div, examples indentation fix,xtype,ytype to support 'string' and 'class java.lang.String' until bridge is in place

--- src/demos/pages/output/charts/numberbar-chart.js
+++ src/demos/pages/output/charts/numberbar-chart.js
@@ -1,8 +1,8 @@
 $('#bar').chart({
-    data: [[[1,2],[3,5],[10,4]]],
-    series:{
-        bars:{show:true}
-    },
-    charttype: 'bar'
+  data: [[[1,2],[3,5],[10,4]]],
+  series:{
+    bars:{show:true}
+  },
+  charttype: 'bar'
 });
 
--- src/demos/pages/output/charts/pie-chart.js
+++ src/demos/pages/output/charts/pie-chart.js
@@ -1,14 +1,11 @@
-$('#pie').chart(
-    {
-        data: [
-                [
-                   {data: 12500746, label: 'Service sector'},
-                   {data: 188217,   label: 'Agricultural sector'},
-                   {data: 2995787, label:  'Industrial sector'}
-                ]
+$('#pie').chart({
+  data: [
+          [
+            {data: 12500746, label: 'Service sector'},
+            {data: 188217,   label: 'Agricultural sector'},
+            {data: 2995787, label:  'Industrial sector'}
+          ]
         ],
-        legend:{sorted:'ascending'},
-        charttype:'pie'
-    }
-
-);
+  legend:{sorted:'ascending'},
+  charttype:'pie'
+});
